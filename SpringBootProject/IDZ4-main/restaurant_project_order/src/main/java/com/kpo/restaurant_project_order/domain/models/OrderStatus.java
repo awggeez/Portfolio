57e1bb05ad9664808014e9aa7781ce3cd3c097af
awggeez <118361120+awggeez@users.noreply.github.com>
2023-10-18 11:23:37 +0300

    Delete SpringBootProject/IDZ4-main directory

--- SpringBootProject/IDZ4-main/restaurant_project_order/src/main/java/com/kpo/restaurant_project_order/domain/models/OrderStatus.java
+++
@@ -1,8 +0,0 @@
-package com.kpo.restaurant_project_order.domain.models;
-
-public enum OrderStatus {
-    WAITING,
-    IN_PROGRESS,
-    COMPLETED,
-    CANCELLED
-}
--- SpringBootProject/IDZ4-main/restaurant_project_order/src/main/java/com/kpo/restaurant_project_order/domain/models/Role.java
+++
@@ -1,7 +0,0 @@
-package com.kpo.restaurant_project_order.domain.models;
-
-public enum Role {
-    CUSTOMER,
-    CHEF,
-    MANAGER
-}
